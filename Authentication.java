7956d5f1013abffe0b4bddb20690310af22bb914
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2022-11-21 20:18:41 +0530

    Update Authentication.java

--- Authentication.java
+++ Authentication.java
@@ -1,7 +1,55 @@
+// Authentication Code
+
+package com.src;
+import java.util.Scanner;
+
 class Authentication {
 	public static void main(String[] args) {
-		System.out.println("");
+		UserAuthOperations UAO = new UserAuthOperations();
+		System.out.println("Log In");
+
+		Scanner sc = new Scanner(System.in);
+		enteredUname = sc.nextLine();
+		UAO.logNewUser(enteredUname);
+		sc.close();
 	}
 }
 
+class UserAuthOperations {
+	private boolean usernameIsValid(String userName) {
+		
+		// return 0 -> Invalid username
+		if (userName.length < 3) {
+			System.out.println("Username too small, needs > 3 characters");
+			return 0;
+		}
+
+		if (userName.length > 40) {
+			System.out.println("Username too large, must have < 40 characters");
+			return 0;
+		}
+
+		if (Character.isDigit(userName.charAt(0))) {
+			System.out.println("Usernames cannot start with a number");
+			return 0;
+		}
+
+		for (char i : userName) {
+			if (!(Character.isAlphaNumeric(i) && i != '_')) {
+				System.out.println("Usernames cannot contain special characters");
+				return 0;
+			}	
+
+			else if (i == ' ') {
+				System.out.println("Usernames cannot contain spaces");
+				return 0;
+			}
+		}
+	}
+
+	private int logNewUser(String userName) {
+		userNameIsValid(userName);
+		// some code
+	}
+}
 
